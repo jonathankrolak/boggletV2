f666db3f5b0859509aa53711e5d7b47cf8ee0dd8
HeresJohnny1722 <jonathan.krolak@icloud.com>
2025-02-27 13:05:40 -0500

    Initial Commit

---
+++ BoggletV2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BoggletV2
+//
+//  Created by HPro2 on 2/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
